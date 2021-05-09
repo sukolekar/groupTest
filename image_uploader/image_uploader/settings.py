9751ba45349056640deb1beec348693eceb4fe46
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2021-05-09 22:45:57 +0530

    Update settings.py

--- image_uploader/image_uploader/settings.py
+++ image_uploader/image_uploader/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_2!rnxbk6wps3rb6)jx!r^lj6*+em8)ns9*pkw^4o#sqc%2pxe'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
